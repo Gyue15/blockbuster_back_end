47e0727ddf7a7484710ee09b83ab5acd76d7d8b8
gyue15 <786330297@qq.com>
2017-12-06 13:22:28 +0800

    post

--- blockbuster/src/main/java/nju/blockbuster/config/ConfigClass.java
+++ blockbuster/src/main/java/nju/blockbuster/config/ConfigClass.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class ConfigClass extends WebMvcConfigurerAdapter {
     public static final String PATH = "/Users/gyue/Pictures/MyPicture/";
+    public static final String AVATAR_PATH = "/Users/gyue/Pictures/MyPicture/";
 
     //供客户端使用的url前缀
     public static final String URL = "/picture/";
@@ -15,6 +16,7 @@ public class ConfigClass extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/picture/**").addResourceLocations("file:"+PATH);
+        registry.addResourceHandler("/avatar/**").addResourceLocations("file:"+AVATAR_PATH);
         super.addResourceHandlers(registry);
     }
 }
--- blockbuster/src/main/java/nju/blockbuster/controller/UserController.java
+++ blockbuster/src/main/java/nju/blockbuster/controller/UserController.java
@@ -1,6 +1,7 @@
 package nju.blockbuster.controller;
 
 import com.alibaba.fastjson.JSON;
+import nju.blockbuster.config.ConfigClass;
 import nju.blockbuster.models.UserModel;
 import nju.blockbuster.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import util.ResultMessage;
 
 @Controller
 @RequestMapping(value = "/user", produces = "application/json;charset=UTF-8")
 public class UserController {
-    private static final String AVATAR = "~/web_project/avatar/avatar.jpg";
 
     @Autowired
     private UserService userService;
@@ -29,7 +30,7 @@ public class UserController {
         }
 
         userModel = new UserModel();
-        userModel.setAvatar(AVATAR);
+        userModel.setAvatar(ConfigClass.AVATAR_PATH);
         userModel.setEmail(email);
         userModel.setPassword(password);
         userModel.setUsername(username);
