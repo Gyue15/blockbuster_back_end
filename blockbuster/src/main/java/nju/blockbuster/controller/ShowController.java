181f6230f2779e1e1e9f57f42c82f75c9301a93d
gyue15 <786330297@qq.com>
2017-12-06 21:44:09 +0800

    更改图片上传

--- blockbuster/src/main/java/nju/blockbuster/controller/ShowController.java
+++ blockbuster/src/main/java/nju/blockbuster/controller/ShowController.java
@@ -1,6 +1,7 @@
 package nju.blockbuster.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.sun.xml.internal.bind.v2.util.QNameMap;
 import nju.blockbuster.config.ConfigClass;
 import nju.blockbuster.models.PhotoModel;
 import nju.blockbuster.models.ShowModel;
@@ -15,32 +16,84 @@ import org.springframework.web.multipart.MultipartFile;
 import util.ResultMessage;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @RequestMapping(value = "/show", produces = "application/json;charset=UTF-8")
 @Controller
 public class ShowController {
 
-    private static final String PARENT_PATH = "~";
-
-    private static final String BROKEN_PATH = "~";
+    private static Map<String, String> filePathMap;
 
     @Autowired
     private ShowService showService;
 
     @PostMapping("/upload")
     @ResponseBody
-    public String upload(MultipartFile file){
-        System.out.println(file);
-        return ("SUCCESS");
+    public String upload(MultipartFile file) {
+        if (filePathMap == null) {
+            filePathMap = new TreeMap<>();
+        }
+        if (!file.isEmpty()) {
+            try {
+                // 文件保存路径
+                String filePath = ConfigClass.PATH + System.currentTimeMillis() + file.getOriginalFilename();
+                // 文件url
+                String fileUrl = ConfigClass.URL + System.currentTimeMillis() + file.getOriginalFilename();
+                File dest = new File(filePath);
+
+                // 检测是否存在目录
+                if (!dest.getParentFile().exists()) {
+                    dest.getParentFile().mkdirs();
+                }
+
+                file.transferTo(dest);
+                filePathMap.put(file.getOriginalFilename(), fileUrl);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(file);
+            return "SUCCESS";
+        }
     }
 
     @PostMapping("/post")
     @ResponseBody
     public String postPhoto(String[] fileNames,String title, String description, String[] tags, String albumId, String email){
-        return ("SUCCESS");
+        Date date = new Date();
+        List<String> fileUrls = new ArrayList<>();
+        for (Map.Entry<String, String> entry: filePathMap.entrySet()) {
+            fileUrls.add(entry.getValue());
+        }
+
+        // 存show表
+        ShowModel showModel = new ShowModel();
+        showModel.setTags(tags);
+        showModel.setDescription(description);
+        showModel.setEmail(email);
+        showModel.setLikeNum(0);
+        showModel.setDate(date);
+        showModel.setAid(albumId);
+        Integer sid = showService.saveShow(showModel);
+        System.out.println("1111111111111111Show Controller: " + sid);
+        if (sid < 0) {
+            return JSON.toJSONString(ResultMessage.FAILURE);
+        }
+
+        // 存Photo表
+        System.out.println("222filename size:" + fileUrls.size());
+        for (int i = 0; i < fileUrls.size(); i++) {
+            PhotoModel photoModel = new PhotoModel();
+            photoModel.setPic(fileNames.get(i));
+            photoModel.setAid(albumId);
+            photoModel.setSid(sid);
+            System.out.println("22222222222222Show Controller2: " + photoModel.getSid());
+            Integer pid = showService.addPhoto(photoModel);
+            if (pid < 0) {
+                return JSON.toJSONString(ResultMessage.FAILURE);
+            }
+        }
+        return JSON.toJSONString(ResultMessage.SUCCESS);
     }
 
 
